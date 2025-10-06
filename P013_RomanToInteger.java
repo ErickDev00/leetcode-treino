224318a97479cecfc9921a4264505acd1d2881ac
Erick <profissional.erick@gmail.com>
2025-10-06 12:11:52 -0300

    013_RomanToInteger

---
+++ P013_RomanToInteger.java
@@ -0,0 +1,45 @@
+public class P013_RomanToInteger {
+
+    private int tradutor(char c){
+        switch (c){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public int romanToInt(String s){
+        int soma = 0;
+
+        int atual = tradutor(s.charAt(0));
+
+        for (int i=1; i < s.length() ;i++){
+            int proximo = tradutor(s.charAt(i));
+
+            if ( atual < proximo){
+                soma -= atual;
+            } else {
+                soma += atual;
+            }
+
+            atual = proximo;
+        }
+
+
+        soma += atual;
+        return soma;
+    }
+}
