fe9a14f155859dbca5bb23ee63183cc5ecadcf44
Erick <profissional.erick@gmail.com>
2025-10-22 14:08:38 -0300

    P083 Remove Duplicates From Sorted List

---
+++ P083_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,22 @@
+public class P083_RemoveDuplicatesFromSortedList {
+
+    /*public ListNode deleteDuplicates(ListNode head) {
+
+        if (head == null) return null;
+
+        ListNode atual = head;
+
+        while(atual.next != null){
+
+            if(atual.val == atual.next.val){
+                atual.next = atual.next.next;
+            } else {
+                atual = atual.next;
+            }
+
+        }
+        return head;
+
+    } */
+
+}
