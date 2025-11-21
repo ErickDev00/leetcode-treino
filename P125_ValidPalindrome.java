bf320bc5956298f04a2fb17db31623b9b56091e3
Erick <profissional.erick@gmail.com>
2025-11-21 12:17:40 -0300

    P125 Valid Palindrome

---
+++ P125_ValidPalindrome.java
@@ -0,0 +1,32 @@
+public class P125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+
+        if (s.length() == 1) {
+            return true;
+        }
+
+        s = s.toLowerCase();
+        int l=0;
+        int r = s.length() - 1;
+
+        while(l < r){
+
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l)) ) {
+                l++;
+            }
+
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r)) ) {
+                r--;
+            }
+
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+
+            l++;
+            r--;
+
+        }
+        return true;
+    }
+}
